a79ac5463890dcb10bf5ce218f5ebc3748d1258b
davidhu34 <b01901133@gmail.com>
2017-02-04 19:22:14 +0800

    mouse scroll zoom

---
+++ Assets/zoom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Mouse ScrollWheel") < transform.position.z - 0.5)
+        {
+            transform.Translate(Vector3.forward * Input.GetAxis("Mouse ScrollWheel"));
+        }
+    }
+}
