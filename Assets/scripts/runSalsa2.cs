274d5a95598b8531fe0eb2f136ceab6c7ac3b767
davidhu34 <b01901133@gmail.com>
2017-02-26 20:52:54 +0800

    female cc

--- Assets/scripts/runSalsa2.cs
+++ Assets/scripts/runSalsa2.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Windows;
 
 using CrazyMinnow.SALSA; // Import SALSA from the CrazyMinnow namespace
@@ -12,22 +13,23 @@ using CrazyMinnow.SALSA.MCS;
 
 public class runSalsa2 : MonoBehaviour {
     public CM_MCSSync mcs;
-    private string clipName = "dialog.wav";
-    private string clipDir;
-    public DateTime oldTime;
-    public AudioClip myAudioClip;
-    public AudioClip myAudioClip2;
-    public WWW www;
-    public WWW www2;
-    public bool making = false;
-    public bool playing = false;
-    public bool started = false;
-    public bool waiting = false;
-    public bool destroyed = false;
-    private Salsa3D salsa3D;
+	private string clipDir;
+	private string txtDir;
+	public DateTime oldTime;
+	public AudioClip myAudioClip;
+	public WWW www;
+	public WWW txtfile;
+	public bool playing = false;
+	public bool started = false;
+	public bool destroyed = false;
+	public bool making = false;
+	private Salsa3D salsa3D;
+	public Animator anim;
+	public RandomEyes3D eyes;
+	public Text answer;
 
-    void makeSalsa() {
-        www = new WWW("file://" + clipDir);
+	void makeSalsa(string name) {
+        www = new WWW("file://" + clipDir + name);
         myAudioClip = www.audioClip;
         Debug.Log(myAudioClip.isReadyToPlay);
         while (!www.isDone) ;
@@ -53,37 +55,45 @@ public class runSalsa2 : MonoBehaviour {
         salsa3D.rangeOfMotion = 100f; // Set the range of motion
         making = false;
         destroyed = false;
-        waiting = true;
     }
     // Use this for initialization
     [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
     void Start()
     {
-        clipDir = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("/")) + "/" + clipName;
-        oldTime = File.GetLastWriteTimeUtc(clipDir);
+		clipDir = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("/")) + "/lib/wavs/";
+		//clipDir = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("/")) + "/wavs/";
+		txtDir = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("/")) + "/wavname.txt";
+		oldTime = File.GetLastWriteTimeUtc(txtDir);
+		txtfile = new WWW("file://" + txtDir);
+		while (!txtfile.isDone) { };
 
-        makeSalsa();
+		anim = GetComponent<Animator>();
+
+		//makeSalsa(txtfile.text);
     }
     // Update is called once per frame
     void Update()
     {
-        if (!playing && waiting && !destroyed)
+        if (!playing && !destroyed)
         {
             Debug.Log("Play");
             playing = true;
+			anim.SetBool("talking", true);
             salsa3D.Play();
         }
         if (destroyed && !salsa3D && !making)
         {
             Debug.Log("waiting for new");
-            DateTime newTime = File.GetLastWriteTimeUtc(clipDir);
+            DateTime newTime = File.GetLastWriteTimeUtc(txtDir);
             if (oldTime != newTime)
             {
                 Debug.Log("oldyime" + oldTime);
                 oldTime = newTime;
                 Debug.Log("newtime" + oldTime);
                 making = true;
-                makeSalsa();
+				txtfile = new WWW("file://" + txtDir);
+				while (!txtfile.isDone) { };
+				makeSalsa(txtfile.text);
             }
         }
     }
@@ -92,9 +102,9 @@ public class runSalsa2 : MonoBehaviour {
         if (playing && !salsa3D.isTalking)
         {
             playing = false;
-            waiting = false;
             Destroy(salsa3D);
             destroyed = true;
+			anim.SetBool("talking", false);
         }
     }
 }
